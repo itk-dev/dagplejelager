fd8577cefc7127b3b3480d590692453e2c1a04cb
Mikkel Ricky <rimi@aarhus.dk>
2023-01-30 16:05:33 +0100

    DAGPLEJ-93: Added order items handling in order view

--- web/modules/custom/dagplejelager_actions/src/TwigExtension/TwigExtension.php
+++ web/modules/custom/dagplejelager_actions/src/TwigExtension/TwigExtension.php
@@ -3,6 +3,7 @@
 namespace Drupal\dagplejelager_actions\TwigExtension;
 
 use Drupal\commerce_order\Entity\Order;
+use Drupal\commerce_order\Entity\OrderItemInterface;
 use Drupal\Core\Session\AccountInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
@@ -31,6 +32,7 @@ class TwigExtension extends AbstractExtension {
   public function getFunctions() {
     return [
       new TwigFunction('dagplejelager_is_granted', [$this, 'isGranted']),
+      new TwigFunction('dagplejelager_order_handling', [$this, 'orderHandling']),
     ];
   }
 
@@ -62,6 +64,37 @@ class TwigExtension extends AbstractExtension {
     }
 
     return FALSE;
+  }
+
+  /**
+   * Return list of handlings on order items.
+   *
+   * @param mixed $orderId
+   *   The order id.
+   *
+   * @return array|string[]
+   *   The handlings.
+   */
+  public function orderHandling($orderId): array {
+    $orderId = (string) $orderId;
+    /** @var ?\Drupal\commerce_order\Entity\OrderInterface $order */
+    $order = Order::load($orderId);
+
+    if (NULL === $order) {
+      return [];
+    }
+
+    return array_filter(
+      array_unique(
+        array_map(
+          static function (OrderItemInterface $item) {
+            return $item->get('field_handling')->value;
+          },
+          $order->getItems()
+        ),
+        SORT_STRING
+      )
+    );
   }
 
 }
